4db8b906f2d027e15dc619ec9c541db8134469f1
JavierGoodall <javiergoodall@outlook.com>
2022-11-08 11:16:39 +0200

    .\https://github.com/JavierGoodall99/JS-Challenge-1

---
+++ js/code.js
@@ -0,0 +1,11 @@
+let yourName = prompt ("What is your name?");
+let yourSurname = prompt ("What is your surname?");
+let Salary = prompt ("What is your salary");
+let bonus = 500
+let totalSalary = eval (`${Salary}+${bonus}`);
+
+
+document.write("Name:" + yourName);
+document.write("<br>Surname:" + yourSurname);
+document.write ("<br>Salary:" + totalSalary)
+
